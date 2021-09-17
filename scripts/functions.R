b2b216df43b57cbb6b8b1e044157ce83cdf08652
NitheshPerumal <nithe175@gmail.com>
2021-09-17 19:58:38 +0000

    Specific cell type list by tissue creation function with quantile pruning

--- scripts/functions.R
+++ scripts/functions.R
@@ -58,6 +58,48 @@ cutoff_summ <- function(x) {
 }
 
 
+#' Summary of Median Cutoffs for many cells
+#' @param x list of objects to pass for cutoff analysis
+#' @return summary table of median pruning
+cutoff_analysis <- function(cell_list) {
+  bp_names <<- c() 
+  z_names <<- c()
+  cpm_1_names <<- c()
+  cpm_0.5_names <<- c()
+  cpm_0.1_names <<- c()
+  cpm_5_names <<- c()
+  cpm_10_names <<- c()
+  
+  out <- data.frame()
+  for (i in cell_list) {
+    command3 <- paste0("out <- rbind(out,cutoff_summ(",i,"$exp))")
+    eval(parse(text=command3))
+  }
+  
+  unique_features <- cbind(as.numeric(table(table(bp_names))[1]),
+                           as.numeric(table(table(z_names))[1]),
+                           as.numeric(table(table(cpm_1_names))[1]),
+                           as.numeric(table(table(cpm_0.5_names))[1]),
+                           as.numeric(table(table(cpm_0.1_names))[1]),
+                           as.numeric(table(table(cpm_5_names))[1]),
+                           as.numeric(table(table(cpm_10_names))[1]))
+  
+  cutoff_analysis <- cbind(as.data.frame(cell_list),out)
+  cutoff_analysis <- rbind(cutoff_analysis,c('unique_features',unique_features))
+  
+  rm(bp_names, pos = 1)
+  rm(z_names, pos = 1)
+  rm(cpm_1_names, pos = 1)
+  rm(cpm_0.1_names, pos = 1)
+  rm(cpm_0.5_names, pos = 1)
+  rm(cpm_10_names, pos = 1)
+  rm(cpm_5_names, pos = 1)
+  
+  return(cutoff_analysis)
+}
+
+
+
 #' Quantile based pruning
 #' Prune features based on quantile cutoff 0.8
 #' @param x datframe of gene expression matrix
@@ -65,11 +107,47 @@ cutoff_summ <- function(x) {
 #' @return pruned dataframe of gene expression matrix with missing features removed
 quant <- function(x,z) {
   count <- as.data.frame(t(as.data.frame(apply(x,1, 
-                                               function(x) unname(quantile(x, c(.8))) 
+                                               function(x) unname(quantile(x, c(.75))) 
                                                ))))
   pruned <- as.data.frame(count[,count >= z])
   out <- as.data.frame(x[names(pruned),])
   return(out)
+}
+
+
+#' Summary of Quantile Cutoffs for many cells
+#' @param x list of objects to pass for quantile analysis
+#' @return summary table of quantile pruning
+quant_analysis <- function(cell_list) {
+  quant_0.1_names <<- c()
+  quant_0.5_names <<- c()
+  quant_1_names <<- c() 
+  quant_5_names <<- c()
+  quant_10_names <<- c()
+  
+  quant_out <- data.frame()
+  for (i in cell_list) {
+    command3 <- paste0("quant_out <- rbind(quant_out,quant_summ(",i,"$exp))")
+    eval(parse(text=command3))
+  }
+  
+  unique_features <- cbind(as.numeric(table(table(quant_1_names))[1]),
+                           as.numeric(table(table(quant_0.5_names))[1]),
+                           as.numeric(table(table(quant_0.1_names))[1]),
+                           as.numeric(table(table(quant_5_names))[1]),
+                           as.numeric(table(table(quant_10_names))[1]))
+  
+  quant_analysis <- cbind(as.data.frame(cell_list), quant_out)
+  quant_analysis <- rbind(quant_analysis,c('unique_features', unique_features))
+  
+
+  rm(quant_1_names, pos = 1)
+  rm(quant_0.1_names, pos = 1)
+  rm(quant_0.5_names, pos = 1)
+  rm(quant_10_names, pos = 1)
+  rm(quant_5_names, pos = 1)
+  
+  return(quant_analysis)
 }
 
 
@@ -298,10 +376,130 @@ filter_dat <- function(exp, met, pcnt = .5, value = 1,
   count_coverage <- signif( 100*(sum(filt_exp)/initial_counts), 4)
   
   
-  return( list(exp = as.data.frame(apply(filt_exp,2,CPM)), 
-               met = met, 
-               features = features, 
+  return( list(exp = as.data.frame(apply(filt_exp,2,CPM)),
+               met = met,
+               features = features,
                count_coverage = count_coverage) )
+  
+}
+
+
+#' Pruned cell types by region 
+#' Prunes features and returns list within list each list is a region with broad
+#' broad cell type and specific cell type with feature names and exp matrix
+#' 
+#' @usage 
+#' 
+#' @param exp An expression data set of raw counts
+#' @param met A metadata object with columnl lables of; region_label, 
+#' external_donor_name_label, broad_cell, cell_type_alias_label, and 
+#' sample_name. These correspond to brain region, donor ID, broad cell type 
+#' label, specific cell type label, and sample name.
+#' @region a character vector of brain region(s)/tissue(s) to subset the data 
+#' by (Optional)
+#' @is.specific boolean to determine if subset with subtypes is wanted, if FALSE
+#' broad cell type will be most specific subset, if TRUE specific cell types
+#' will be most specific cell type
+#' @result c('feature_names','exp_matrix') feature names or expression matrix
+#' output determined
+#' 
+#' @return a list object by region (list$region), broad cell type object 
+#' (list$broad cell type), specific cell type object (list$ specific cell type), 
+#' feature_name returns list of feature names while exp_matrix returns the 
+#' expression matrix for the subset after quantile pruning of 25% 
+#' (list$feature_name) or (list$exp_matrix)
+#' 
+#' @export 
+#' 
+
+prune_quant <- function(exp, met, region, is.specific = NULL,
+                  result = c('feature_name','exp_matrix')) {
+  if (missing(exp)) {
+    stop(paste0('Must specify expression matrix'))
+  }
+  if (missing(met)) {
+    stop(paste0('Must specify meta dataframe'))
+  }
+  if (missing(region)) {
+    stop(paste0('Must specify a region'))
+  }
+  if (missing(is.specific)) {
+    stop(paste0('Must specify is.specific with boolean value'))
+  }
+
+  
+  output_list <- c()
+  spec_list <- c()
+if (result == 'feature_name') {
+      if(is.specific == FALSE){
+        for (i in unique(met$broad_cell)) {
+        data <- filter_dat(exp = exp, met = met, pcnt = 0.5, value = 1,
+                           region = region, is.broad = TRUE, cell_t = i)
+        command <- paste0(
+          'output_list <- c(output_list,"', i,'"= list(rownames(quant(data$exp,0.5))))'
+        )
+        eval(parse(text=command))
+
+      }
+    }else{
+
+      for (i in unique(met$broad_cell)) {
+      data <- filter_dat(exp = exp, met = met, pcnt = 0.5, value = 1, 
+                         region = region, is.broad = TRUE, cell_t = i)
+      
+      for (j in unique(data$met$cell_type_alias_label)) {
+            
+        data_sub <- filter_dat(exp = exp, met = met, pcnt = 0.5, value = 1, 
+                              is.broad = FALSE, cell_t = j)
+        command <- paste0(
+          'spec_list <- c(spec_list,"', j,'"= list(rownames(quant(data_sub$exp,0.5))))'
+        )
+        eval(parse(text=command))
+        }
+          
+        command1 <- paste0(
+        'output_list <- c(output_list,"', i,'"= list(spec_list))'
+        )
+        eval(parse(text=command1))
+      }
+    }
+}else{
+  
+  if(is.specific == FALSE){
+    for (i in unique(met$broad_cell)) {
+      data <- filter_dat(exp = exp, met = met, pcnt = 0.5, value = 1,
+                         region = region, is.broad = TRUE, cell_t = i)
+      command <- paste0(
+        'output_list <- c(output_list,"', i,'"= list(quant(data$exp,0.5)))'
+      )
+      eval(parse(text=command))
+      
+    }
+  }else{
+    
+    for (i in unique(met$broad_cell)) {
+      data <- filter_dat(exp = exp, met = met, pcnt = 0.5, value = 1, 
+                         region = region, is.broad = TRUE, cell_t = i)
+      
+      for (j in unique(data$met$cell_type_alias_label)) {
+        
+        data_sub <- filter_dat(exp = exp, met = met, pcnt = 0.5, value = 1, 
+                               is.broad = FALSE, cell_t = j)
+        command <- paste0(
+          'spec_list <- c(spec_list,"', j,'"= list(quant(data_sub$exp,0.5)))'
+        )
+        eval(parse(text=command))
+      }
+      
+      command1 <- paste0(
+        'output_list <- c(output_list,"', i,'"= list(spec_list))'
+      )
+      eval(parse(text=command1))
+    }
+  }
+ }
+  
+  return(output_list)
   
 }
 
@@ -341,7 +539,7 @@ composition <- function(cell_list, mainbar_name, sets_name) {
   colnames(composition)[1] <- 'features'
   
   # UpSet plot data
-  ups <- features
+  ups <- features[,2:(ncol(features)-1)]
   ups[ups != 0] <- 1
   
   return( list(comp = composition,
